ba27f37c579d7b1d08bf5ad03ed4b81d4b995727
Adonis Rebano <earebano01@monccnb.ca>
2023-12-14 15:10:48 -0400

    plante connecte mini-projet final version

--- plantConnectV2_22_11_2023/appPlante/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppplanteConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'appPlante'
--- plantConnectV2_22_11_2023/appPlante/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import BASE
-urlpatterns = [
-    path('', BASE, name='BASE'),
-
-]
